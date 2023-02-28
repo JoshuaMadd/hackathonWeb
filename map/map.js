fbd38a09f6f4561a575de57d4bda1827b7450049
JoshuaMadd <joshua.maddelein@student.vives.be>
2023-02-28 13:03:47 +0100

    added map

---
+++ map/map.js
@@ -0,0 +1,31 @@
+const setup = () => {
+    loadFloorChange();
+    initNodes();
+}
+
+const loadFloorChange = () =>{
+    let opts = document.getElementsByClassName("opt");
+    opts[0].addEventListener("click", changeFloor);
+    opts[1].addEventListener("click", changeFloor);
+    opts[2].addEventListener("click", changeFloor);
+}
+
+const changeFloor = (event) =>{
+    let button = event.target;
+    let oldButton = document.getElementsByClassName("selected")[0];
+    oldButton.classList.remove("selected")
+    button.classList.add("selected")
+
+    let mapId = button.innerText - 1;
+    let oldMap = document.querySelector('img[style*="display: block;"]');
+    oldMap.style.display = "none";
+    let selector = 'img[src*="floorplan/'+ mapId +'.png"]';
+    let newMap = document.querySelector(selector);
+    newMap.style.display = "block";
+}
+
+const initNodes = () =>{
+    
+}
+
+window.addEventListener("load",setup);
